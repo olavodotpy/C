f5079c3ceef230dc6010379e49106522c4c46598
olavodotpy <olavoazevedo88@gmail.com>
2025-01-27 20:31:46 -0300

    add loops, size, increment and pre increment operations and boolean operations

--- src/logic/whileAndDowhile.c
+++ src/logic/whileAndDowhile.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+int numberGenerator(int number) {
+
+    int r;
+
+    do {
+        r = rand() % 100;
+        printf("%d this is different from that %d\n", r, number);
+    } while (r != number);
+}
 
 
 int main(void) {
-    int i = 0;
+    int i = 10;
 
     while (i < 10) {
         printf("i is now %d!\n", i);
         i++;
     }
 
+    do {
+        printf("do-while: i is %d\n", i);
+        i++;
+    } while (i < 10);
+
+    numberGenerator(50);
+
     printf("all done!\n");
 
-    while (1) {
-        printf("repeats forever\n");
-    }
+    // while (1) {
+    //     printf("repeats forever\n");
+    // }
 }
