37c7e710f489dd5f688c6e832945f61d448a9849
olavodotpy <olavoazevedo88@gmail.com>
2025-01-28 16:05:20 -0300

    error in is prime in c

--- src/logic/forLoop.c
+++ src/logic/forLoop.c
@@ -5,7 +5,7 @@ int main(void) {
     int i;
     int j;
 
-    for (i = 0, j= 10; i < 2, j < 30; i++, j++) {
+    for (i = 0, j = 10; i < 2, j < 30; i++, j++) {
     // a virgula serve para separar as expressões e executalos em 1 só
     // da esquerda para a direita, sempre priorizando o mais da direita
         printf("%d, %d\n", i, j);
