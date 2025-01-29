2662cfb9f7ccf194592d24c302b73e45939e8df6
olavodotpy <olavoazevedo88@gmail.com>
2025-01-29 16:48:40 -0300

    isPrime

---
+++ src/logic/isPrime.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+bool isPrime(int n){
+
+    if (n <= 1) {
+        return false;
+    }
+
+    int divisors = 0; 
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            divisors ++;
+        }
+    }
+
+    return (divisors == 2); 
+
+}
+
+int main(void) {
+    bool result = isPrime(5);
+
+    printf(result ? "Is a prime\n" : "Is not a prime\n");
+}
